a251325c2d93867b812bc6183f1ce502b0e99156
anthony peiris <mapeiris@hotmail.com>
2022-04-05 10:40:21 -0400

    HX711 loadcell

---
+++ src/timerInterrupt.cpp
@@ -0,0 +1,40 @@
+#include "esp32-hal-timer.h"
+volatile int interruptCounter;
+int totalInterruptCounter;
+ 
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+ 
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+ 
+}
+ 
+void setup() {
+ 
+
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 100, true);
+  timerAlarmEnable(timer);
+ 
+}
+ 
+void loop() {
+ 
+  if (interruptCounter > 0) {
+ 
+    portENTER_CRITICAL(&timerMux);
+    interruptCounter--;
+    portEXIT_CRITICAL(&timerMux);
+ 
+    totalInterruptCounter++;
+ 
+    printf("An interrupt %i as occurred. Total number: %i\n",interruptCounter,totalInterruptCounter);
+    
+ 
+  }
+}
